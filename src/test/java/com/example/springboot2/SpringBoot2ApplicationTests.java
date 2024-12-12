dae47d0d580532af6055e6c80392f2e3a6f3c1fe
Rajeev <rranjan@infoane.com>
2024-12-12 21:23:26 +0530

    Making it web applicaion and adding test cases.

--- src/test/java/com/example/springboot2/SpringBoot2ApplicationTests.java
+++ src/test/java/com/example/springboot2/SpringBoot2ApplicationTests.java
@@ -3,11 +3,25 @@ package com.example.springboot2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloController.class)
 class SpringBoot2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testHelloEndpoint() throws Exception {
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("index")) // Checks if the view name is "index"
+				.andExpect(model().attribute("message", "Hello, World!")); // Checks the model attribute
+	}
 }
